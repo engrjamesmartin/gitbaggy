d08885b372c582e4b96168fbae6953a3c954acbc
baganalytics <engr.jamesmartin@gmail.com>
2018-08-16 08:18:12 -0700

    Some sudo code on what I need to do. Working on Twitter first

---
+++ gitbaggy.py
@@ -0,0 +1,6 @@
+#need to have my library for twitter already created
+#neet to have my library for tensorflow stuff created
+
+#pass to functions in my twitter lib
+
+#pass to functions in my tensorflow lib
